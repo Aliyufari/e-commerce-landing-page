b4d29e936cdc16649f60a1725283407f6ba7110d
Aliyu Abubakar <aliyufari@gmail.com>
2025-03-04 00:38:22 +0100

    Added Responsive for the Nav

--- src/pages/components/Navigation.tsx
+++ src/pages/components/Navigation.tsx
@@ -1,34 +1,41 @@
-import { Heart, MagnifyingGlass, ShoppingCart, User } from "@phosphor-icons/react"
+import { Heart, List, MagnifyingGlass, ShoppingCart, User } from "@phosphor-icons/react"
 
 const Navigation = () => {
   return (
-    <nav className="flex items-center justify-between px-32 pt-8 pb-4">
-        <h2 className="font-bold text-xl">Exclusive</h2>
+    <nav className="flex items-center justify-center">
+        <div className="w-[85vw] flex items-center justify-between pt-8 pb-4">
+            <h2 className="font-bold text-xl">Exclusive</h2>
 
-        <ul className="flex items-center gap-8">
-            <li className="border-b border-gray-300">Home</li>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Sign Up</li>
-        </ul>
+            <ul className="hidden items-center gap-10 md:flex">
+                <li className="border-b border-gray-300">Home</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Sign Up</li>
+            </ul>
 
-        <section className="flex items-center gap-5">
-            <form className="flex items-center gap-1 bg-gray-200 px-4 py-2 rounded">
-                <input type="text" id="search" className="bg-inherit outline-none text-xs" placeholder="What are you looking for?" />
-                <label htmlFor="search"><MagnifyingGlass size={22} /></label>
-            </form>
-            
-            <Heart size={22} />
+            <section className="flex items-center gap-6">
+                <form className="hidden items-center bg-gray-200 px-3 py-2 rounded lg:flex">
+                    <input 
+                        type="text" 
+                        id="search" 
+                        className="bg-inherit outline-none text-xs focus:bg-inherit" 
+                        placeholder="What are you looking for?" />
+                    <label htmlFor="search"><MagnifyingGlass size={22} /></label>
+                </form>
+                
+                <Heart size={22} />
 
-            <span className="relative">
-                <ShoppingCart size={22} />
-                <span className="flex items-center justify-center text-white w-4 h-4 rounded-full bg-[#DB4444] text-xs absolute -top-1 -right-2">2</span>
-            </span>
+                <span className="relative">
+                    <ShoppingCart size={22} />
+                    <span className="flex items-center justify-center text-white w-4 h-4 rounded-full bg-[#DB4444] text-xs absolute -top-1 -right-2">2</span>
+                </span>
 
-            <span className="flex items-center justify-center w-7 h-7 rounded-full bg-[#DB4444]">
-                <User className="text-white" size={18} />
-            </span>
-        </section>
+                <span className="flex items-center justify-center w-7 h-7 rounded-full bg-[#DB4444]">
+                    <User className="text-white" size={18} />
+                </span>
+                <List size={32} className="block md:hidden" />
+            </section>
+        </div>
     </nav>
   )
 }
--- src/pages/components/TopHeader.tsx
+++ src/pages/components/TopHeader.tsx
@@ -2,16 +2,18 @@ import { CaretDown } from "@phosphor-icons/react"
 
 const TopHeader = () => {
   return (
-    <div className="bg-gray-950 h-12 flex items-center justify-between text-md px-32">
-        <div></div>
-        <p className="text-gray-50">
+    <section className="bg-gray-950 h-12 flex items-center justify-center text-sm">
+      <div className="flex items-center justify-center gap-4 w-[85vw] md:flex-row md:justify-end">
+        <p className="hidden text-gray-50 text-center mx-auto md:block">
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! 
-            <span className="font-semibold ml-4">Shop Now</span>
+            <strong className="ml-4">Shop Now</strong>
         </p>
-        <button className="flex items-center text-gray-50 cursor-buttonointer">
-            English <CaretDown className="text-gray-50 ml-2" size={16} weight="bold" />
-        </button>
-    </div>
+        <select className="flex items-center gap-2 text-gray-50 text-sm">
+          <option value="">English</option>
+          <option value="">Hausa</option>
+        </select>
+      </div>
+    </section>
   )
 }
 
